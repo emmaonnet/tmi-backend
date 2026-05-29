763c4e51e6e5ad762402638c84e93c43083d339c
emmaonnet <emmaonnet@gmail.com>
2026-05-29 23:28:06 +0100

    Remove unused express and cors imports
    Removed express and cors imports from server.js.

--- server.js
+++ server.js
@@ -1753,12 +1753,12 @@ app.get("/api/health", (req, res) => {
 
 
 
-const express = require("express");
+
 const http = require("http");
 const { Server } = require("socket.io");
-const cors = require("cors");
 
-const app = express();
+
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
